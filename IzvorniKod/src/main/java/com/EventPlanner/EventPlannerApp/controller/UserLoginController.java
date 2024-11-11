986aac90a27ed09c80e7e8147e7fc48df26b59f8
CognitoAgent <nina.karavanic@fer.hr>
2024-11-11 11:52:27 +0100

    Cross Origin dodan u UserLoginController klasi
    U klasi UserControllerLogin dodan je @ CrossOrigin kako bi mogao komunicirati s frontendom

--- IzvorniKod/src/main/java/com/EventPlanner/EventPlannerApp/controller/UserLoginController.java
+++ IzvorniKod/src/main/java/com/EventPlanner/EventPlannerApp/controller/UserLoginController.java
@@ -1,13 +1,14 @@
 package com.EventPlanner.EventPlannerApp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.EventPlanner.EventPlannerApp.domain.User;
 import com.EventPlanner.EventPlannerApp.service.UserService;
-
+@CrossOrigin(origins = "http://localhost:3000") // assuming React runs on port 3000
 @RestController
 public class UserLoginController {
 	
