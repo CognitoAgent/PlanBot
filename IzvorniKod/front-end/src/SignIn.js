5f2e20a88fea7689292d0296b6e8f4168b2e439a
tomabegovic21 <toma.begovic@fer.hr>
2024-11-13 10:23:43 +0100

    Uređivanje koda

--- IzvorniKod/front-end/src/SignIn.js
+++ IzvorniKod/front-end/src/SignIn.js
@@ -4,113 +4,98 @@ import FormHeader from './components/FormHeader'
 import FormFooter from './components/FormFooter';
 
 import './FormStyle.css';
-function Form(){
-    const [inputs, setInputs]=useState({})
-    function handleChange(e){
-        const name=e.target.name;
-        const value=e.target.value;
-        setInputs({...inputs, [name]:value});
+function Form() {
+    const [inputs, setInputs] = useState({})
+    function handleChange(e) {
+        const name = e.target.name;
+        const value = e.target.value;
+        setInputs({ ...inputs, [name]: value });
     }
-    /*
-    function handleEnter(e){
-        if(e.target.name==="Username"){
-            document.getElementsByName('Password')[0].focus();
-        }
-        if(e.target.name==="Password"){
-            // To je isto kao i pritisak na Sign in
-        }
-
-    }
-     */   
-    function handleSubmit(event){
-        //localhost:8080/login
+    function handleSubmit(event) {
         event.preventDefault();
-        if(inputs.username==='' || inputs.username===undefined || inputs.username===null){
+        if (inputs.username === '' || inputs.username === undefined || inputs.username === null) {
             alert('Please enter user name');
             document.getElementsByName('username')[0].focus();
         }
-        else if(inputs.password===''|| inputs.password===undefined || inputs.password===null){
+        else if (inputs.password === '' || inputs.password === undefined || inputs.password === null) {
             alert('Please enter password');
             document.getElementsByName('password')[0].focus();
         }
-        else{
-        fetch('http://localhost:8080/login', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(inputs),
-        })
-        .then(response => {
-            
-            if (response.ok) {
-                return response.text();
-               // window.location.replace('http://localhost:3000/AdminPanel');
-                //return response.text(); // Assuming your backend returns a token as a string
-            }
-            else{
-            throw new Error("Login failed");
-            }
-        })
-        .then(text =>{
-            if(text===''){
-                alert("Response body is empty");
-                throw  new Error("Response body is empty");
-            }
-            return text;
-        })
-        .then(text =>{
-            sessionStorage.setItem('token',text); 
-            window.location.replace('http://localhost:3000/AdminPanel');
-        })
-        //.then(data => alert("Login Successful: " + data)) // Or handle token storage here
-        .catch(error => alert(error.message));
-    }
-        
+        else {
+            fetch('http://localhost:8080/login', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(inputs),
+            })
+                .then(response => {
+
+                    if (response.ok) {
+                        return response.text();
+                    }
+                    else {
+                        throw new Error("Login failed");
+                    }
+                })
+                .then(text => {
+                    if (text === '') {
+                        throw new Error("Response body is empty");
+                    }
+                    return text;
+                })
+                .then(text => {
+                    sessionStorage.setItem('token', text);
+                    window.location.replace('http://localhost:3000/AdminPanel');
+                })
+                .catch(error => alert(error.message));
+        }
+
     }
     return (
-        <form className="form" onSubmit ={handleSubmit} onKeyDown={e => {if(e.key==="Enter") return}} style={{
+        <form className="form" onSubmit={handleSubmit} onKeyDown={e => { if (e.key === "Enter") return }} style={{
             padding: "0%",
             display: "flex",
-            flexDirection:" column",
+            flexDirection: " column",
             justifyContent: "space-between",
-            width:"100%",
-            marginTop:"20px",
-            height:"200px"}}>
-            <FormElement type="text" name="username" display="User name"placeHolder='Username' value={inputs.username}
-             onChange={handleChange} />
-            <FormElement type="password" name="password" display="Password" placeHolder='Password' value={inputs.password} onChange={handleChange} />
-          {/*   onChange={handleChange}  onKeyDown={(e)=>{if(e.key==="Enter")handleEnter(e)}}*/}
-        <input type="submit" value="Sign in" className="submitButton formElement"
-            style={{  
-                boxSizing: "border-box",
-                marginLeft: "auto",
-                marginRight: "auto",
-                display: "block",
-                paddingLeft: "5px",
-                borderRadius: "4px",
-                width:"100%" ,
-                height: "35px",
-                border: "1px solid black",
-                backgroundColor: "black",
-                 color:"white"
+            width: "100%",
+            marginTop: "20px",
+            height: "200px"
+        }}>
+            <FormElement type="text" name="username" display="User name" placeHolder='Username' value={inputs.username}
+                onChange={handleChange} />
+            <FormElement type="password" name="password" display="Password" placeHolder='Password' value={inputs.password}
+                onChange={handleChange} />
+            <input type="submit" value="Sign in" className="submitButton formElement"
+                style={{
+                    boxSizing: "border-box",
+                    marginLeft: "auto",
+                    marginRight: "auto",
+                    display: "block",
+                    paddingLeft: "5px",
+                    borderRadius: "4px",
+                    width: "100%",
+                    height: "35px",
+                    border: "1px solid black",
+                    backgroundColor: "black",
+                    color: "white"
                 }}
-        />
+            />
         </form>
     );
 }
-function SignIn(){
-    return(
+function SignIn() {
+    return (
         <div className='all' style={{
-            height:"300px", width:"250px",
+            height: "300px", width: "250px",
             marginLeft: "auto",
             marginRight: "auto",
             padding: "2%",
             border: "1px solid black",
             borderRadius: "4px",
-           backgroundColor: "white",
+            backgroundColor: "white",
         }}>
-        <FormHeader heading="Sign in" text="Sign in to your account"/>
-        <Form/>
-        <FormFooter question="Don't have an account? " href="/Register" link="Register"/>
+            <FormHeader heading="Sign in" text="Sign in to your account" />
+            <Form />
+            <FormFooter question="Don't have an account? " href="/Register" link="Register" />
         </div>
     );
 }
