97cebc8fb0479b5d28a8fdb8ed84c561e354562e
CognitoAgent <nina.karavanic@fer.hr>
2024-11-14 19:29:01 +0100

    +
    +

--- IzvorniKod/src/main/java/com/EventPlanner/EventPlannerApp/WebConfig.java
+++ IzvorniKod/src/main/java/com/EventPlanner/EventPlannerApp/WebConfig.java
@@ -9,6 +9,9 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("https://planbot-9s64.onrender.com/").allowedMethods("POST", "GET", "PUT", "DELETE");
+        registry.addMapping("/**").allowedOrigins("https://planbot-9s64.onrender.com/")
+        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+        .allowedHeaders("*")
+        .allowCredentials(true);
     }
 }
