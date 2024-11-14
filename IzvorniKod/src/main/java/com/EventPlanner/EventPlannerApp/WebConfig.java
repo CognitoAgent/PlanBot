1f2fdd0ca780a25c9e5bb95fb9bcb843a0013059
CognitoAgent <nina.karavanic@fer.hr>
2024-11-15 00:50:45 +0100

    WebConfig congifured
    WebConfig promijenjen, sada dopusta OPTIONS metodu i dopusteni su neki headeri

--- IzvorniKod/src/main/java/com/EventPlanner/EventPlannerApp/WebConfig.java
+++ IzvorniKod/src/main/java/com/EventPlanner/EventPlannerApp/WebConfig.java
@@ -7,12 +7,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://planbot-9s64.onrender.com")
-                .allowedMethods("POST", "GET", "PUT", "DELETE")
-                .allowCredentials(true)
-                .allowedHeaders("*");
+                .allowedOrigins("https://planbot-9s64.onrender.com")  // Frontend URL
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("Content-Type", "Authorization")
+                .allowCredentials(true); // Allow cookies/credentials
     }
 }
