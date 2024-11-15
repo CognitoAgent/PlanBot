ee4072e770a86ce7b7f1ade4f9ec635fef37dec4
CognitoAgent <nina.karavanic@fer.hr>
2024-11-15 19:37:10 +0100

    +
    +

--- IzvorniKod/src/main/java/com/EventPlanner/EventPlannerApp/WebConfig.java
+++ IzvorniKod/src/main/java/com/EventPlanner/EventPlannerApp/WebConfig.java
@@ -1,5 +1,5 @@
 package com.EventPlanner.EventPlannerApp;
-
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,13 +7,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("https://planbot-9s64.onrender.com")  // Frontend URL
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("Content-Type", "Authorization")
-                .allowCredentials(true); // Allow cookies/credentials
+        registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowedMethods("POST", "GET", "PUT", "DELETE");
     }
 }
--- IzvorniKod/src/main/java/com/EventPlanner/EventPlannerApp/service/MyUserDetailsService.java
+++ IzvorniKod/src/main/java/com/EventPlanner/EventPlannerApp/service/MyUserDetailsService.java
@@ -1,7 +1,5 @@
 package com.EventPlanner.EventPlannerApp.service;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -14,8 +12,6 @@ import com.EventPlanner.EventPlannerApp.repo.UserRepo;
 
 @Service
 public class MyUserDetailsService implements UserDetailsService{
-	
-	private static final Logger logger = LoggerFactory.getLogger(MyUserDetailsService.class);
 
 	@Autowired
 	private UserRepo repo;
@@ -23,12 +19,10 @@ public class MyUserDetailsService implements UserDetailsService{
 	@Override //we need a layer that will fetch data for this class: UserRepo
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		//getting username, have to check if username exists in db
-		logger.info("Searching for user: " + username);
 		System.out.println("Searching for user: " + username);
 		User user = repo.findByUsername(username);
 		
 		if(user==null) {
-			logger.error("User not found: " + username);
 			System.out.println("User not found");
 			throw new UsernameNotFoundException("user not found");
 		}
