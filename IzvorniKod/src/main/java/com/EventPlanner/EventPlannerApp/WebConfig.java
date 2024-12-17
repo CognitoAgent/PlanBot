fa7d12e9709f7fd7da18fb095bfcb3daec767386
CognitoAgent <nina.karavanic@fer.hr>
2024-12-17 12:36:23 +0100

    Webconfig komentiran

--- IzvorniKod/src/main/java/com/EventPlanner/EventPlannerApp/WebConfig.java
+++ IzvorniKod/src/main/java/com/EventPlanner/EventPlannerApp/WebConfig.java
@@ -3,7 +3,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+/*
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
@@ -12,3 +12,4 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**").allowedOrigins("https://planbot-9s64.onrender.com").allowedMethods("POST", "GET", "PUT", "DELETE");
     }
 }
+*/
