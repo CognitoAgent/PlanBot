39f8192d454aeb8932de59c88ea5fbb0f60813c1
CognitoAgent <nina.karavanic@fer.hr>
2024-12-24 19:40:02 +0100

    Koristenje aws secret-a za povezivanje s bazom + napomena
    Kreiran aws secret, dodani Access Key ID i Secret Access Key za dohvacanje secreta iz kojeg se procita url, username i password za bazu koja se nalazi na aidenu
    
    NAPOMENA(2): - kreiran je Access Key ID i Secret Access Key za backend koja se lokalno pokrece; moguce da je poslije potrebno kreirati nove za backend koji je deployan
                            - za Access Key ID i Secret Access Key stavljeni su placeholderi, procitani su iz env. variables

--- IzvorniKod/src/main/java/com/EventPlanner/EventPlannerApp/secretConfig/AWSSecretsManagerConfig.java
+++ IzvorniKod/src/main/java/com/EventPlanner/EventPlannerApp/secretConfig/AWSSecretsManagerConfig.java
@@ -2,7 +2,10 @@ package com.EventPlanner.EventPlannerApp.secretConfig;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.beans.factory.annotation.Value;
+
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
 import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
@@ -11,22 +14,35 @@ import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRespon
 @Configuration
 public class AWSSecretsManagerConfig {
 
-   // @Value("${"eu-central-1"}")
-    //private String awsRegion;
+   @Value("${aws.region}")
+    private String awsRegion;
+   
+   @Value("${aws.accessKeyId}")
+   private String accessKeyId;
 
-    public String getSecret(String secretName) {
-    	//secretName = "event/secrets";
-        try (SecretsManagerClient client = SecretsManagerClient.builder()
-                .region(Region.of("eu-central-1"))
-                .credentialsProvider(DefaultCredentialsProvider.create())
-                .build()) {
+   @Value("${aws.secretAccessKey}")
+   private String secretAccessKey;
+   
+   @Value("${aws.secret.name}")
+   private String secretName;
 
-            GetSecretValueRequest getSecretValueRequest = GetSecretValueRequest.builder()
-                    .secretId(secretName)
-                    .build();
+    public String getSecret() {
+    	System.out.println("Dobiveni accesskey: "+accessKeyId);
+    	System.out.println("Dobiveni secretAccesskey: "+secretAccessKey);
+    	AwsBasicCredentials awsCredentials = AwsBasicCredentials.create(accessKeyId, secretAccessKey);
+    	
+    	SecretsManagerClient client = SecretsManagerClient.builder()
+    			.region(Region.of(awsRegion))
+    			.credentialsProvider(StaticCredentialsProvider.create(awsCredentials))
+    			.build();
+    	
+    	GetSecretValueRequest request = GetSecretValueRequest.builder()
+    			.secretId(secretName)
+    			.build();
+    	
+    	GetSecretValueResponse response = client.getSecretValue(request);
+    	return response.secretString();
 
-            GetSecretValueResponse response = client.getSecretValue(getSecretValueRequest);
-            return response.secretString();
-        }
+         
     }
 }
--- IzvorniKod/src/main/java/com/EventPlanner/EventPlannerApp/secretConfig/DatabaseConfig.java
+++ IzvorniKod/src/main/java/com/EventPlanner/EventPlannerApp/secretConfig/DatabaseConfig.java
@@ -3,9 +3,13 @@ package com.EventPlanner.EventPlannerApp.secretConfig;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
+
+import javax.sql.DataSource;
 
 @Component
 public class DatabaseConfig {
@@ -14,11 +18,29 @@ public class DatabaseConfig {
     private AWSSecretsManagerConfig secretsManagerConfig;
 
     @Value("${aws.secret.name}")
-    private String secretName = "event/secrets";
+    private String secretName;
 
     public Map<String, String> getDatabaseCredentials() throws Exception {
-        String secretString = secretsManagerConfig.getSecret(secretName);
+        String secretString = secretsManagerConfig.getSecret();
         ObjectMapper objectMapper = new ObjectMapper();
         return objectMapper.readValue(secretString, Map.class);
+    }
+    
+    @Bean
+    public DataSource dataSource() throws Exception {
+        Map<String, String> credentials = getDatabaseCredentials();
+        
+        System.out.println("Credentials");
+        for(Map.Entry<String, String> e: credentials.entrySet()) {
+        	System.out.println("key: "+e.getKey()+", value: "+e.getValue());
+        }
+
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        //dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl(credentials.get("DATABASE_URL"));
+        dataSource.setUsername(credentials.get("DATABASE_USERNAME"));
+        dataSource.setPassword(credentials.get("DATABASE_PASSWORD"));
+
+        return dataSource;
     }
 }
