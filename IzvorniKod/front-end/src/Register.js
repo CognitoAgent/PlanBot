3f493057c2971d76778dc34cd1f5a4bdfe16fa59
Toma Begović <toma.begovic@fer.hr>
2024-12-27 23:03:47 +0100

    novi link

--- IzvorniKod/front-end/src/Register.js
+++ IzvorniKod/front-end/src/Register.js
@@ -36,7 +36,7 @@ function Form() {
             document.getElementsByName('password')[0].focus();
         }
         else {
-            fetch('https://52.213.213.5:8080/register', {
+            fetch('/api/register', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(inputs),
