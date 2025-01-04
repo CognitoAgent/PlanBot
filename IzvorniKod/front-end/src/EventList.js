ceaffe14f2e840ddf89df179a13f6f80148ae1db
tomabegovic21 <85010785+tomabegovic@users.noreply.github.com>
2025-01-04 11:34:13 +0100

    Dodan zahtjev za dohvaćanje događaja

--- IzvorniKod/front-end/src/EventList.js
+++ IzvorniKod/front-end/src/EventList.js
@@ -4,14 +4,47 @@ import MyEvent from "./components/EventComponents/MyEvent";
 import { useState } from 'react';
 function EventList(){
     const[selected,setSelected]=useState("My events");
+    let events=[];
     function handleChange(event){
         setSelected(event.target.value);
     }
+    /*
     let dog={date:"20/02/2025",location:"FER",description:"Sastanak na FER-u", title:"Sastanak"};
     let events=[<Event event={dog}/>,<Event event={dog}/>,<Event event={dog}/>,<Event event={dog}/>,<Event event={dog}/>,<Event event={dog}/>
     
         ,<Event event={dog}/>,<Event event={{date:"20/02/2025",location:"FER",description:"Sastanak na FER-u", title:"Sastanak"}}/>,<Event event={dog}/>,<Event event={dog}/>,<Event event={dog}/>,<Event event={dog}/>,<Event event={dog}/>,<Event event={dog}/>];
         let myEvents=[<MyEvent event={dog}/>,<MyEvent event={dog}/>,<MyEvent event={dog}/>,<MyEvent event={dog}/>,<MyEvent event={dog}/>,<MyEvent event={dog}/>]
+        */
+       
+        const token = sessionStorage.getItem("token");
+        if(token===null){
+            window.location.replace('/login');
+        }
+        else{
+        fetch('https://52.213.213.5:8443/eventlist', {
+            method: 'POST',
+            headers: { 
+                'Content-Type': 'application/json', 
+                'Authorization': `Bearer ${token}` 
+            },
+            body: selected
+        })
+            .then(response => {
+                if (response.ok) {
+                    events=response.json();
+                } else {
+                    throw new Error("Loading events is not possible");
+                }
+            })
+            .catch(error => alert(error.message));
+            
+        }
+        events.map(e => {
+            if(selected==="My events"){
+                return <MyEvent event={e}/>
+            }
+            return <Event event={e}/>
+        });
         return(
         <>
         <div style={{width:"1166px", marginLeft:"auto", marginRight:"auto"}}>
@@ -27,7 +60,7 @@ function EventList(){
 
         </div>
         <div style={{ display:"flex",flexWrap:"wrap",alignItems:"flex-start",  gap:"40px",padding:"0%",   height: events.length>=9?"80vh" : "50vh", width:"1166px", marginLeft:"auto",marginRight:"auto"}}>
-            {selected==="Other events"? events: myEvents}
+            {events}
         </div>
         </>
     )
