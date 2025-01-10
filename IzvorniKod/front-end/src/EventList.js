c1dbcd1b5967257aafc240bb6127a49848b410c0
tomabegovic21 <toma.begovic@fer.hr>
2025-01-10 18:55:51 +0100

    debugiranje

--- IzvorniKod/front-end/src/EventList.js
+++ IzvorniKod/front-end/src/EventList.js
@@ -40,7 +40,7 @@ function EventList(){
             .then(r => {events=r;
                 let i=0;
                 for(i=0;i<events.length;i++){
-                    alert(events[i].title);
+                    alert("Naslov je" + events[i].title);
                     alert(events[i].location);
                 }
                 })
