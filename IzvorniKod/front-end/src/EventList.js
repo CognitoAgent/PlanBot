d278d840edb3e7af6c528c45c6a7344c79689e34
tomabegovic21 <toma.begovic@fer.hr>
2025-01-13 11:59:01 +0100

    Popravljen tekst za gumb koji vodi na published events

--- IzvorniKod/front-end/src/EventList.js
+++ IzvorniKod/front-end/src/EventList.js
@@ -85,7 +85,7 @@ function EventList(){
         <Button text="New Event" onClick={()=>window.location.replace('adminpanel')}/>
         </div>
     <div style={{ width: "1166px", marginLeft: "auto", marginRight: "auto", marginTop: "10px" }}>
-                <Button text="Accepted events" onClick={() => window.location.replace('publishedevents')} />
+                <Button text="My events" onClick={() => window.location.replace('publishedevents')} />
             </div>
         <div style={{ display:"flex",flexWrap:"wrap",alignItems:"flex-start",  gap:"40px",padding:"0%",   height: events.length>=9?"80vh" : "50vh", width:"1166px", marginLeft:"auto",marginRight:"auto"}}>
             {events}
