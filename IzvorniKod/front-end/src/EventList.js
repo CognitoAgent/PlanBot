fa99b6cc5a5d7d7b525489d459231f4dfa42d17b
tomabegovic21 <toma.begovic@fer.hr>
2025-01-13 15:31:28 +0100

    novi link

--- IzvorniKod/front-end/src/EventList.js
+++ IzvorniKod/front-end/src/EventList.js
@@ -8,7 +8,7 @@ function EventList(){
     const [events,setEvents]=useState([]);
     useEffect(()=>{
        // alert(selected);
-        fetch('https://52.213.213.5:8443/eventlist', {
+        fetch('https://ec2-52-30-64-126.eu-west-1.compute.amazonaws.com:8443/eventlist', {
             method: 'POST',
             headers: { 
                 'Content-Type': 'application/json', 
