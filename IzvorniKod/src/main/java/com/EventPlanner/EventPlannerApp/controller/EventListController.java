5733319683a53e609553d12b178e7e470078361d
CognitoAgent <nina.karavanic@fer.hr>
2025-01-13 13:19:35 +0100

    dorada eventlist i publishedevents stranice
    getEventList dohavaca potrebne objave
    dodana proposeBtn za mogucnost dodavanja predloska novog datuma i/ili lokacije
    Dodan JPARepository za dohvacanja Suggestion-sa

--- IzvorniKod/src/main/java/com/EventPlanner/EventPlannerApp/controller/EventListController.java
+++ IzvorniKod/src/main/java/com/EventPlanner/EventPlannerApp/controller/EventListController.java
@@ -3,8 +3,10 @@ package com.EventPlanner.EventPlannerApp.controller;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,7 +14,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.EventPlanner.EventPlannerApp.domain.Post;
+import com.EventPlanner.EventPlannerApp.domain.Suggestion;
 import com.EventPlanner.EventPlannerApp.repo.PostRepo;
+import com.EventPlanner.EventPlannerApp.repo.SuggestionRepo;
 import com.EventPlanner.EventPlannerApp.repo.UserRepo;
 import com.EventPlanner.EventPlannerApp.service.PostService;
 import com.EventPlanner.EventPlannerApp.service.UserService;
@@ -29,6 +33,9 @@ public class EventListController {
 
 	@Autowired
 	private PostRepo postRepo;
+	
+	@Autowired
+	private SuggestionRepo suggestionRepo;
 
 	@Autowired
 	private UserService service;
@@ -56,33 +63,39 @@ public class EventListController {
 	        	System.out.println("Iz baze smo dobili "+allPosts.size()+" objava");
 	        }
 
-	        List<fakePost> allFakePosts = new LinkedList<>();
-            for (Post p : allPosts) {
-            	fakePost fp = new fakePost(p.getId(), p.getTitle(), p.getDate(), p.getLocation(),
-                		p.getDescription(), p.getPicture(), p.getPublishedBy().getUsername(), p.getComments(),
-                		p.getSuggestions());
-                
-                allFakePosts.add(fp);
-                
-            }
-            System.out.println("Kreirali allFakePosts listu, ima "+allFakePosts.size()+" objava");
+	        
 
-	        if (selected.trim().contains("My events")) {
+	        if (selected.trim().contains("Accepted events")) {
 	            //System.out.println("Selected je jednak My events");
-	            List<fakePost> myFakePosts = new LinkedList<>();
-	            System.out.println("Trebamo samo objave korisnika; radimo myFakePosts listu");
-	            for (fakePost p : allFakePosts) {
-	                if (p.getPublishedBy().equals(service.getCurrentUsername())) {
-	                    myFakePosts.add(p);
+	            List<fakePost> joinedFakePosts = new LinkedList<>();
+	            System.out.println("Trebamo samo objave koje je korisnik prihvatio; "
+	            			+ "radimo joinedFakePosts listu");
+	            for (Post p : allPosts) {
+	                if (p.getJoinedBy().contains(service.getCurrentUser())) {
+	                	fakePost fp = new fakePost(p.getId(), p.getTitle(), p.getDate(),
+	                			p.getLocation(), p.getDescription(), p.getPicture(),
+	                			p.getPublishedBy().getUsername(), p.getComments(), p.getSuggestions());
+	                	joinedFakePosts.add(fp);
+	                    
 	                }
 	            }
-	            System.out.println("myFakePosts ima "+myFakePosts.size()+" objava, to su sljedece");
-	            for(fakePost p: myFakePosts) {
+	            System.out.println("joinedFakePosts ima "+joinedFakePosts.size()+" objava, to su sljedece");
+	            for(fakePost p: joinedFakePosts) {
 	            	System.out.println(p.toString());
 	            }
 	            
-	            return ResponseEntity.ok(myFakePosts);
-	        } else if (selected.trim().contains("Other events")) {
+	            return ResponseEntity.ok(joinedFakePosts);
+	        } else if (selected.trim().contains("All events")) {
+	        	List<fakePost> allFakePosts = new LinkedList<>();
+	            for (Post p : allPosts) {
+	            	fakePost fp = new fakePost(p.getId(), p.getTitle(), p.getDate(), p.getLocation(),
+	                		p.getDescription(), p.getPicture(), p.getPublishedBy().getUsername(), p.getComments(),
+	                		p.getSuggestions());
+	                
+	                allFakePosts.add(fp);
+	                
+	            }
+
 	        	System.out.println("Saljemo allFakePosts listu koja ima "
 	        						+allFakePosts.size()+" objava za prikaz");
 	            return ResponseEntity.ok(allFakePosts);
@@ -95,4 +108,33 @@ public class EventListController {
 	    }
 	}
 	
+	@PostMapping("/proposechange")
+    public ResponseEntity<String> proposeBtn(@RequestBody Post proposal) throws Throwable{
+        try {
+            // Get the currently logged-in user's ID
+            Long userId = service.getCurrentUserId();
+
+            if (userId == null) {
+                return ResponseEntity.status(403).body("Unauthorized access");
+            }
+
+            // Find the post to be updated
+            Optional<Post> optionalPost = postRepo.findById(proposal.getId());
+            if (optionalPost.isEmpty()) {
+				return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Event not found");
+			}
+            Post post = optionalPost.get();
+
+            Suggestion s = new Suggestion(proposal.getDate(), proposal.getLocation());
+            suggestionRepo.save(s);
+            post.getSuggestions().add(s);
+            
+            // Save the updated post
+            postRepo.save(post);
+
+            return ResponseEntity.ok("Post updated successfully");
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(400).body(e.getMessage());
+        }//try/catch
+	}
 }
