9aae9fb348e685679cc5c0db2cceb5f7941247df
tomabegovic21 <toma.begovic@fer.hr>
2025-01-13 13:52:26 +0100

    pamćenje statusa prihvaćenosti

--- IzvorniKod/front-end/src/components/EventComponents/Event.js
+++ IzvorniKod/front-end/src/components/EventComponents/Event.js
@@ -2,7 +2,7 @@ import Button from "./Button";
 import Footer from "./Footer";
 import { useState } from 'react';
 function Event({event}){
-    const[accepted,setAccepted]=useState(false);
+    const[accepted,setAccepted]=useState(event.accepted || false);
     function proposeChange(){
         sessionStorage.setItem('event',JSON.stringify(event));
         window.location.replace('proposechange');
