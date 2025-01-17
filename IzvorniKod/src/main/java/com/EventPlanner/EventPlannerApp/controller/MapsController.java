d4fdf3ea519fb5328abf75e36954e2fda2e5abd1
jogalic <josip.galic2@fer.unizg.hr>
2025-01-17 11:14:02 +0100

    Dodan MapController
    Dodan controller za Google Maps

---
+++ IzvorniKod/src/main/java/com/EventPlanner/EventPlannerApp/controller/MapsController.java
@@ -0,0 +1,26 @@
+package com.EventPlanner.EventPlannerApp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+
+@CrossOrigin(origins = "https://planbot-9s64.onrender.com")
+@RestController
+public class MapsController {
+    @Value("${google.maps.api.key}")
+    private String googleMapsApiKey;
+
+    @GetMapping("/api/maps-key")
+    public ResponseEntity<?> getMapsApiKey() {
+        if (googleMapsApiKey == null || googleMapsApiKey.isEmpty()) {
+            return ResponseEntity.status(500).body("Google Maps API key is not configured");
+        }
+        return ResponseEntity.ok("{\"apiKey\": \"" + googleMapsApiKey + "\"}");
+    }
+}
