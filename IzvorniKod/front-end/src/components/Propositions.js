5e0a79f7cd348d8b51091802e2568b9aef8a6660
tomabegovic21 <toma.begovic@fer.hr>
2025-01-17 15:07:36 +0100

    debugiranje

--- IzvorniKod/front-end/src/components/Propositions.js
+++ IzvorniKod/front-end/src/components/Propositions.js
@@ -19,8 +19,8 @@ function Propositions(){
     sessionStorage.removeItem('propositions');
     sessionStorage.removeItem('comments');
     */
-alert(propositions[0].date);
-    propositions=[{date:"datum", location:"lokacija"},{date:"datum", location:"lokacija"},{date:"datum", location:"lokacija"}]
+alert(propositions[0].location);
+    //propositions=[{date:"datum", location:"lokacija"},{date:"datum", location:"lokacija"},{date:"datum", location:"lokacija"}]
      comments=["Ovo je komentar ","Ovo je komentar ","Ovo je komentar ","Ovo je komentar "]
     
    if(propositions!=null){
