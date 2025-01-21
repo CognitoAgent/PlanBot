5a20b3e92a4702e9f6029bf08c0282c657953322
tomabegovic21 <toma.begovic@fer.hr>
2025-01-21 20:12:25 +0100

    css

--- IzvorniKod/front-end/src/components/EventComponents/Button.js
+++ IzvorniKod/front-end/src/components/EventComponents/Button.js
@@ -1,6 +1,6 @@
 function Button({text, onClick, style}){
     return(
-     <button onClick={onClick} style={{...style, borderRadius:"4px", backgroundColor: "black", color:"white",paddingLeft:"15px",paddingRight:"15px",height: "40px", fontSize:"medium"}}>
+     <button onClick={onClick} style={{...style, borderRadius:"4px",border:"1px solid #002d6e", backgroundColor: "#002d6e", color:"white",paddingLeft:"15px",paddingRight:"15px",height: "40px", fontSize:"medium"}}>
         {text}
     </button>
 )};
